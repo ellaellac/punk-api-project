6675f7a1419e91816e57422f2a87a0ba2ed2afd9
ellaellac <ellachoywy@gmail.com>
2022-12-13 15:21:52 +0000

    Beer API

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,33 @@ import "./App.scss";
 // import NavBar from "./containers/NavBar/NavBar";
 import CardList from "./containers/CardList/CardList";
 import NavBar from "./containers/NavBar/NavBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import beers from "./data/beers";
-
-console.log(beers);
+// import beers from "./data/beers";
 
 const App = () => {
   //Name Search
   const [searchTerm, setSearchTerm] = useState("");
+  //API - Beer
+  const [beers, setBeers] = useState();
+  console.log(beers);
+
+  const getAllBeers = async () => {
+    const url = "https://api.punkapi.com/v2/beers";
+    const response = await fetch(url);
+    const data = await response.json();
+    setBeers(data);
+  };
+
+  useEffect(() => {
+    getAllBeers();
+  }, []);
 
   return (
     <>
       <p> Testing </p>
       <NavBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <CardList beers={beers} searchTerm={searchTerm} />
+      {beers && <CardList beers={beers} searchTerm={searchTerm} />}
     </>
   );
 };
